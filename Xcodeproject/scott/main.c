be9cf64ba505fcfbd25e869e965bc6e21aa3f9da
angstsmurf <ignalina@me.com>
2022-01-28 04:56:38 +0100

    Add new ScottFree

---
+++ Xcodeproject/scott/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  scott
+//
+//  Created by Administrator on 2022-01-27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
